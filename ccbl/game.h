b211a8fd473ed5efb3278c1dbc177ed038fabae0
Andrii Tabala <atabala@student.unit.ua>
2019-03-28 17:34:51 +0200

    y

---
+++ ccbl/game.h
@@ -0,0 +1,14 @@
+#ifndef GAME_H
+# define GAME_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 	s_pr
+{
+	
+}				t_pr;
+
+#endif
---
+++ ccbl/main.c
@@ -0,0 +1,8 @@
+#include "game.h"
+
+int int main(int argc, char **argv)
+{
+	t_pr	g;
+	printf("%s\n", "hey there");
+	return (0);
+}
