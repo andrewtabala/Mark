819cb9688c5fb2e0df496bdbdc4faea2f0488878
Andrii Tabala <atabala@student.unit.ua>
2019-03-29 15:32:56 +0200

    minor fixes

--- cbl/keyactions.c
+++ cbl/keyactions.c
@@ -13,23 +13,19 @@ void		move(int i, t_pr *g)
 	mlx_clear_window(g->mlx_ptr, g->win_ptr);
 	if (i == 123 || i == 0)
 	{
+		g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guy2.xpm", &g->w, &g->w);
+		if (g->lev >= 3)
+			g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace2.xpm", &g->w, &g->w);
 		if (g->xguy > 0)
-		{
-			g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guy2.xpm", &g->w, &g->w);
-			if (g->lev >= 3)
-				g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace2.xpm", &g->w, &g->w);
 			g->xguy -= 8;
-		}
 	}
 	else
 	{
+		g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guy.xpm", &g->w, &g->w);
+		if (g->lev >= 3)
+			g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace.xpm", &g->w, &g->w);
 		if (g->xguy < g->w_width - 64)
-		{
-			g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guy.xpm", &g->w, &g->w);
-			if (g->lev >= 3)
-				g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace.xpm", &g->w, &g->w);
 			g->xguy += 8;
-		}
 	}
 	g->ybull += 6;
 	g->points += 5;
