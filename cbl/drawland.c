24168800c723c42981a235d8d866aecb886fd91c
Andrii Tabala <atabala@student.unit.ua>
2019-03-29 13:47:31 +0200

    levels

--- cbl/drawland.c
+++ cbl/drawland.c
@@ -2,7 +2,7 @@
 
 void	drawdrop(t_pr *g, int x, int y)
 {
-	g->img_proj = mlx_xpm_file_to_image(g->mlx_ptr, "textures/bullet.xpm", &g->w, &g->w);
+	g->img_proj = mlx_xpm_file_to_image(g->mlx_ptr, "textures/bullet1.xpm", &g->w, &g->w);
 	mlx_put_image_to_window(g->mlx_ptr, g->win_ptr, g->img_proj, 40, g->ybull);
 }
 
@@ -13,22 +13,29 @@ void	drawguy(t_pr *g)
 
 void	drawback(t_pr *g)
 {
-	int i = 0;
-	int j = 0;
-	int x = 0;
-	int y = 0;
-	int s = 512;
-	g->img_back = mlx_xpm_file_to_image(g->mlx_ptr, "textures/sky.xpm", &s, &s);
-			mlx_put_image_to_window(g->mlx_ptr, g->win_ptr, g->img_back, x, y);
+	if (g->lev == 1)
+		g->img_back = mlx_xpm_file_to_image(g->mlx_ptr, "textures/back1.xpm", &g->w, &g->w);
+	else if (g->lev == 2)
+		g->img_back = mlx_xpm_file_to_image(g->mlx_ptr, "textures/back2.xpm", &g->w, &g->w);
+	else if (g->lev == 3)
+		g->img_back = mlx_xpm_file_to_image(g->mlx_ptr, "textures/back3.xpm", &g->w, &g->w);
+	else
+		g->img_back = mlx_xpm_file_to_image(g->mlx_ptr, "textures/back4.xpm", &g->w, &g->w);
+	mlx_put_image_to_window(g->mlx_ptr, g->win_ptr, g->img_back, 0, 0);
 }
 
 void	drawland(t_pr *g)
 {
 	int 	i = 0;
+	int 	c = 480;
 	if (g->lev == 1)
-		g->img_land = mlx_xpm_file_to_image(g->mlx_ptr, "textures/wood.xpm", &g->w, &g->w);
+		g->img_land = mlx_xpm_file_to_image(g->mlx_ptr, "textures/grass.xpm", &g->w, &g->w);
 	else if (g->lev == 2)
-		g->img_land = mlx_xpm_file_to_image(g->mlx_ptr, "textures/redbrick.xpm", &g->w, &g->w);
+	{
+		g->img_land = mlx_xpm_file_to_image(g->mlx_ptr, "textures/cloud.xpm", &g->w, &g->w);
+		mlx_put_image_to_window(g->mlx_ptr, g->win_ptr, g->img_land, 0, g->w_height - 160);
+		return;
+	}
 	else if (g->lev == 3)
 		g->img_land = mlx_xpm_file_to_image(g->mlx_ptr, "textures/stone.xpm", &g->w, &g->w);
 	else
