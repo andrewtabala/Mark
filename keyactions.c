34b689cbce31140410bc6038de7f1fe161ba65f8
Artur-yurii KORCHYNSKYI <akorchyn@e2r10p1.unit.ua>
2019-03-30 19:50:37 +0200

    fix

--- keyactions.c
+++ keyactions.c
@@ -87,7 +87,7 @@ void		move(int i, t_pr *g)
 			(g->menubar == 1 || i == 0) ? g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace2.xpm", &g->w, &g->w) : 0;
 			(g->menubar == 2 && i == 123) ? (g->img_teammate = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace2.xpm", &g->w, &g->w)) : 0;
 		}
-		if ((g->xguy > 0 && i == 0) || g->menubar == 1)
+		if (g->xguy > 0 && (i == 0 || g->menubar == 1))
 			g->xguy -= 8;
 		else if (g->menubar == 2 && g->xteammate > 0 && i == 123)
 			g->xteammate -= 8;
@@ -101,7 +101,7 @@ void		move(int i, t_pr *g)
 			(g->menubar == 1 || i == 2) ? g->img_guy = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace.xpm", &g->w, &g->w) : 0;
 			(g->menubar == 2 && i == 124) ? (g->img_teammate = mlx_xpm_file_to_image(g->mlx_ptr, "textures/guyspace.xpm", &g->w, &g->w)) : 0;
 		}
-		if ((g->xguy < g->w_width - 64 && i == 2) || g->menubar == 1)
+		if (g->xguy < g->w_width - 64 && (i == 2 || g->menubar == 1))
 			g->xguy += 8;
 		else if (g->menubar == 2 && g->xteammate < g->w_width - 64 && i == 124)
 			g->xteammate += 8;
